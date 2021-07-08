c39dd59c433e7ab2178f0fc1dfc1ff19888804e9
kahfiaulia <kahfiaulia39@gmail.com>
2021-07-08 20:52:37 +0700

    tambah index baru, file output

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,23 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('index');
+	public function index() {
+		$this->load->view("index");
 	}
+	
+	public function data_channel() {
+		// $data = array(
+		// 'id' => $this->input->post('id_channel'),
+		// 'maxResults' => $this->input->post('max_results')
+		// );
+		
+		// $this->load->view("output", $data);
+
+		$data = $this->input->post();
+		print_r($data);
+		}
+
+	public function data_video() {
+		// $data = array(
+		// 'idVideo' => $this->input->post('id_video')
+		// );
+		
+		// $this->load->view("output", $data);
+
+		$data = $this->input->post();
+		print_r($data);
+		}
 }
