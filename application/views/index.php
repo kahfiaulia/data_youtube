ef80a289815684fef36d0d82ec59a8c3f30e6cd1
kahfiaulia <kahfiaulia39@gmail.com>
2021-07-28 11:46:52 +0700

    bg

--- application/views/index.php
+++ application/views/index.php
@@ -90,7 +90,7 @@ hr.solid {
   border-top: 3px solid #bbb;
 }
 body {
-  background-image: url('/data_youtube-1/assets/bg.jpg');
+  background-image: url('/data_youtube/assets/bg.jpg');
   background-repeat: no-repeatbg;
   background-attachment: fixed;
   background-size: cover;
