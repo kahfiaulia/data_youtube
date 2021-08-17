84584b3c2ce4bc8a7be95e54eb6cff92d17ca692
kahfiaulia <kahfiaulia39@gmail.com>
2021-08-17 13:54:27 +0700

    hapus base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/data_youtube/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
