eda8cfe17fc50ade82a760b03a367d268f22227d
kahfiaulia <kahfiaulia39@gmail.com>
2021-08-17 19:16:32 +0700

    tambah title&metadata di head index

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data Channel YouTube</title>
 </head>
 <!-- navbar -->
 <nav class="navbar navbar-dark bg-dark navbar fixed-top ">
